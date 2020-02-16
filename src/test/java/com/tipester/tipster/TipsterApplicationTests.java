3f53c316b0e4b81714fe6de01c61802cc55526e4
Bartłomiej Dettlaff <bartlomiejdettlaff@interia.pl>
2020-02-16 17:01:03 +0100

    Commit z projektem

---
+++ src/test/java/com/tipester/tipster/TipsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tipester.tipster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
