8473440987837dd942adc0b071eca10f1f2eef73
Bartłomiej Dettlaff <bartlomiejdettlaff@interia.pl>
2020-03-25 00:32:21 +0100

    Skończenie pracy nad listą w changelog.

---
+++ src/main/java/com/tipester/tipster/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.tipester.tipster.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(Arrays.asList(
+                "OPTIONS", "GET", "POST", "PUT", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+
+}
--- src/main/java/com/tipester/tipster/endpoints/manageModule/ChangeLogEndpoint.java
+++ src/main/java/com/tipester/tipster/endpoints/manageModule/ChangeLogEndpoint.java
@@ -7,15 +7,12 @@ import com.tipester.tipster.services.manageModule.ChangeLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@CrossOrigin("*")
 public class ChangeLogEndpoint {
 
     private ChangeLogService changeLogService;
